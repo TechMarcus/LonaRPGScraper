8325374663d2a06efb44108809913875014a1885
TechMarcus <ranger.ahilles@gmail.com>
2026-03-11 19:56:52 +0200

    fixed scraping mechanism

--- get_game/get_game.py
+++ get_game/get_game.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 def get_game(filepath, download_path):
-  mainFolder = '1-AbAeaaIuW9jrCFy6UQ9w7t2l-R_cOGD'
+  mainFolder = '1eKAJ1W8PB6wjnjRZQjjM-8sSM5GHKGOy'
   files = get_files(mainFolder)
   df = pd.DataFrame(files)
 
@@ -13,29 +13,48 @@ def get_game(filepath, download_path):
     try:
       files = get_files(latest_version[1])
       df = pd.DataFrame(files)
-      latest_version = find_latest_version(files, df)
+      if len(files) == 0:
+        break
+      new_latest_version = find_latest_version(files, df)
+      latest_version = new_latest_version
     except Exception as e:
-      print("An error occurred: ", e)
+      print("An error occurred: on version search:", e)
       break
-    
+ 
   new_version = is_new_version(latest_version[0], filepath)
+  print("latest version:", latest_version[0], latest_version)
   if new_version is not None:
-    download(latest_version[1], latest_version[0], download_path)
+    try:
+      download(latest_version[1], latest_version[0], download_path)
+    except Exception as e:
+      print("An error occurred: on download:", e)
+      return
     with open(filepath, 'w') as f:
       f.write(str(new_version))
     print("new version is avalible")
 
 
 def get_files(folder):
-    query = f"parents = '{folder}'"
-    response = service.files().list(q=query).execute()
-    files = response.get('files')
+    query = f"'{folder}' in parents"
+    response = service.files().list(
+        q=query,
+        pageSize=1000,
+        supportsAllDrives=True,
+        includeItemsFromAllDrives=True
+    ).execute()
+    files = response.get('files', [])
     nextPageToken = response.get('nextPageToken')
 
     while nextPageToken:
-      response = service.files().list(q=query).execute()
-      files.extend(response.get('files'))
-      nextPageToken = response.get('nextPageToken')
+        response = service.files().list(
+            q=query,
+            pageToken=nextPageToken,
+            pageSize=1000,
+            supportsAllDrives=True,
+            includeItemsFromAllDrives=True
+        ).execute()
+        files.extend(response.get('files', []))
+        nextPageToken = response.get('nextPageToken')
     return files
 
 
@@ -56,13 +75,17 @@ def version_to_number(version):
 
 def find_latest_version(files, df):
   versions = []
-  for file in files:
-      versions.append(version_to_number(file['name']))
-  latest_version = [
-    max(versions),
-    df['id'].values[versions.index(max(versions))]
-  ]
-  return latest_version
+  try:
+    for file in files:
+        versions.append(version_to_number(file['name']))
+    latest_version = [
+      max(versions),
+      df['id'].values[versions.index(max(versions))]
+    ]
+    return latest_version
+  except Exception as e:
+    print("An error occurred: on latest version search:", e)
+    return []
 
 
 def download(id, name, download_path):
